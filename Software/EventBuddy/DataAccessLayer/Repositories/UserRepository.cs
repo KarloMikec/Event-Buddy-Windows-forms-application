24e2d6037b81a28df6f2a9839418e3dc65bb28ef
Karlo Mikec <karlo.mikec18@gmail.com>
2024-01-25 23:01:37 +0100

    changed name of form 1 to frmLogin

--- Software/EventBuddy/DataAccessLayer/Repositories/UserRepository.cs
+++ Software/EventBuddy/DataAccessLayer/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntitiesLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class UserRepository
-    {
+    public class UserRepository : Repository<korisnik>
+    { // getUser metoda
+        public UserRepository(EventBuddyModel context) : base(context)
+        {
+        }
     }
 }
