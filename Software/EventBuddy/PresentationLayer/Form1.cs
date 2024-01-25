9efff36fd2cd052668c6bb970472c91d7a9b2f68
Karlo Mikec <karlo.mikec18@gmail.com>
2024-01-25 22:33:00 +0100

    Fixed tipfeler when typing

--- Software/EventBuddy/PresentationLayer/Form1.cs
+++ Software/EventBuddy/PresentationLayer/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace PresentationLayer
 {
     public partial class Form1 : Form
-    {S
+    {
         public Form1()
         {
             InitializeComponent();
