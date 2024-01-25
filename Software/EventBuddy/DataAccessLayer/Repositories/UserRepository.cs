696ccef9c5893f0f80b02c7295014b4cf87e2ec0
Sebastijan Vinko <svinko21@foi.hr>
2024-01-25 22:44:13 +0100

    Implemented basic hiding of an event #4
    Added HideEventFrm with 3 buttons. Added EventRepository and UserRepository. Added EventServices and UserServices. Implemented basic hiding of an event with warning the organizer.

---
+++ Software/EventBuddy/DataAccessLayer/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class UserRepository : Repository<korisnik>
+    {
+        public UserRepository(EventBuddyModel context) : base(context)
+        {
+        }
+
+        public override int Update(korisnik entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int warnUser(int userID, bool saveChanges = true)
+        {
+            var user = Entities.SingleOrDefault(d => d.ID == userID);
+            user.upozorenja += 1;
+
+            if (saveChanges)
+            {
+                return SaveChanges();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Software/EventBuddy/PresentationLayer/forms/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PresentationLayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvEvents = new System.Windows.Forms.DataGridView();
+            this.btnSakrij = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEvents
+            // 
+            this.dgvEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEvents.Location = new System.Drawing.Point(12, 12);
+            this.dgvEvents.Name = "dgvEvents";
+            this.dgvEvents.Size = new System.Drawing.Size(1193, 597);
+            this.dgvEvents.TabIndex = 0;
+            // 
+            // btnSakrij
+            // 
+            this.btnSakrij.Location = new System.Drawing.Point(500, 639);
+            this.btnSakrij.Name = "btnSakrij";
+            this.btnSakrij.Size = new System.Drawing.Size(334, 50);
+            this.btnSakrij.TabIndex = 1;
+            this.btnSakrij.Text = "Sakrij";
+            this.btnSakrij.UseVisualStyleBackColor = true;
+            this.btnSakrij.Click += new System.EventHandler(this.btnSakrij_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 701);
+            this.Controls.Add(this.btnSakrij);
+            this.Controls.Add(this.dgvEvents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvents)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEvents;
+        private System.Windows.Forms.Button btnSakrij;
+    }
+}
+
