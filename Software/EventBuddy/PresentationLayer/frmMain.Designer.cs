d36912feba851baeafae5ba13809a17f59bfdf8f
Dominik Josipović <djosipovi21@student.foi.hr>
2024-01-25 22:15:34 +0100

    Created design for main form after login #3

---
+++ Software/EventBuddy/PresentationLayer/frmMain.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PresentationLayer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnProfile = new System.Windows.Forms.Button();
+            this.dgvEvents = new System.Windows.Forms.DataGridView();
+            this.Događaji = new System.Windows.Forms.Label();
+            this.btnCategories = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDismissEvent = new System.Windows.Forms.Button();
+            this.btnHideEvent = new System.Windows.Forms.Button();
+            this.btnParticipants = new System.Windows.Forms.Button();
+            this.btnSaveAsPDF = new System.Windows.Forms.Button();
+            this.cmbLocation = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(144, 26);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(162, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 26);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Pretraži";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Datum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(492, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mjesto";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(305, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(181, 26);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // btnProfile
+            // 
+            this.btnProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProfile.Location = new System.Drawing.Point(690, 12);
+            this.btnProfile.Name = "btnProfile";
+            this.btnProfile.Size = new System.Drawing.Size(98, 39);
+            this.btnProfile.TabIndex = 5;
+            this.btnProfile.Text = "Profil";
+            this.btnProfile.UseVisualStyleBackColor = true;
+            // 
+            // dgvEvents
+            // 
+            this.dgvEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEvents.Location = new System.Drawing.Point(12, 117);
+            this.dgvEvents.Name = "dgvEvents";
+            this.dgvEvents.Size = new System.Drawing.Size(606, 321);
+            this.dgvEvents.TabIndex = 6;
+            // 
+            // Događaji
+            // 
+            this.Događaji.AutoSize = true;
+            this.Događaji.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Događaji.Location = new System.Drawing.Point(12, 94);
+            this.Događaji.Name = "Događaji";
+            this.Događaji.Size = new System.Drawing.Size(72, 20);
+            this.Događaji.TabIndex = 7;
+            this.Događaji.Text = "Događaji";
+            // 
+            // btnCategories
+            // 
+            this.btnCategories.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategories.Location = new System.Drawing.Point(624, 391);
+            this.btnCategories.Name = "btnCategories";
+            this.btnCategories.Size = new System.Drawing.Size(164, 47);
+            this.btnCategories.TabIndex = 13;
+            this.btnCategories.Text = "Kategorije";
+            this.btnCategories.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(624, 338);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(164, 47);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Obriši događaj";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(624, 285);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(164, 47);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Izmjeni podatke";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDismissEvent
+            // 
+            this.btnDismissEvent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDismissEvent.Location = new System.Drawing.Point(624, 232);
+            this.btnDismissEvent.Name = "btnDismissEvent";
+            this.btnDismissEvent.Size = new System.Drawing.Size(164, 47);
+            this.btnDismissEvent.TabIndex = 16;
+            this.btnDismissEvent.Text = "Obustavi";
+            this.btnDismissEvent.UseVisualStyleBackColor = true;
+            // 
+            // btnHideEvent
+            // 
+            this.btnHideEvent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHideEvent.Location = new System.Drawing.Point(624, 179);
+            this.btnHideEvent.Name = "btnHideEvent";
+            this.btnHideEvent.Size = new System.Drawing.Size(164, 47);
+            this.btnHideEvent.TabIndex = 17;
+            this.btnHideEvent.Text = "Sakrij događaj";
+            this.btnHideEvent.UseVisualStyleBackColor = true;
+            // 
+            // btnParticipants
+            // 
+            this.btnParticipants.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParticipants.Location = new System.Drawing.Point(624, 126);
+            this.btnParticipants.Name = "btnParticipants";
+            this.btnParticipants.Size = new System.Drawing.Size(164, 47);
+            this.btnParticipants.TabIndex = 18;
+            this.btnParticipants.Text = "Sudionici";
+            this.btnParticipants.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveAsPDF
+            // 
+            this.btnSaveAsPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveAsPDF.Location = new System.Drawing.Point(471, 81);
+            this.btnSaveAsPDF.Name = "btnSaveAsPDF";
+            this.btnSaveAsPDF.Size = new System.Drawing.Size(147, 30);
+            this.btnSaveAsPDF.TabIndex = 19;
+            this.btnSaveAsPDF.Text = "Spremi kao PDF";
+            this.btnSaveAsPDF.UseVisualStyleBackColor = true;
+            // 
+            // cmbLocation
+            // 
+            this.cmbLocation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbLocation.FormattingEnabled = true;
+            this.cmbLocation.Location = new System.Drawing.Point(545, 10);
+            this.cmbLocation.Name = "cmbLocation";
+            this.cmbLocation.Size = new System.Drawing.Size(127, 28);
+            this.cmbLocation.TabIndex = 20;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbLocation);
+            this.Controls.Add(this.btnSaveAsPDF);
+            this.Controls.Add(this.btnParticipants);
+            this.Controls.Add(this.btnHideEvent);
+            this.Controls.Add(this.btnDismissEvent);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCategories);
+            this.Controls.Add(this.Događaji);
+            this.Controls.Add(this.dgvEvents);
+            this.Controls.Add(this.btnProfile);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnProfile;
+        private System.Windows.Forms.DataGridView dgvEvents;
+        private System.Windows.Forms.Label Događaji;
+        private System.Windows.Forms.Button btnCategories;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDismissEvent;
+        private System.Windows.Forms.Button btnHideEvent;
+        private System.Windows.Forms.Button btnParticipants;
+        private System.Windows.Forms.Button btnSaveAsPDF;
+        private System.Windows.Forms.ComboBox cmbLocation;
+    }
+}
