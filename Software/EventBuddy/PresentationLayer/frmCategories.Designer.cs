65f5ad45ce5d12c899f655ddc1e4982fedfcbb63
Karlo Mikec <karlo.mikec18@gmail.com>
2024-01-28 16:37:20 +0100

    Form for displaying elements with all associated elements has been created. Also a form for new categories with all associated elements was created.

---
+++ Software/EventBuddy/PresentationLayer/frmCategories.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PresentationLayer
+{
+    partial class frmCategories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvCategory = new System.Windows.Forms.DataGridView();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.btnSaveAsPDF = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDeleteCategory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(417, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(472, 73);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(169, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Pretraži";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // dgvCategory
+            // 
+            this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategory.Location = new System.Drawing.Point(27, 142);
+            this.dgvCategory.Name = "dgvCategory";
+            this.dgvCategory.Size = new System.Drawing.Size(417, 260);
+            this.dgvCategory.TabIndex = 2;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblCategory.Location = new System.Drawing.Point(23, 119);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(80, 20);
+            this.lblCategory.TabIndex = 3;
+            this.lblCategory.Text = "Kategorije";
+            // 
+            // btnSaveAsPDF
+            // 
+            this.btnSaveAsPDF.Location = new System.Drawing.Point(327, 109);
+            this.btnSaveAsPDF.Name = "btnSaveAsPDF";
+            this.btnSaveAsPDF.Size = new System.Drawing.Size(117, 27);
+            this.btnSaveAsPDF.TabIndex = 4;
+            this.btnSaveAsPDF.Text = "Spremi kao PDF";
+            this.btnSaveAsPDF.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(472, 142);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(169, 41);
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRequest.Location = new System.Drawing.Point(472, 189);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(169, 41);
+            this.btnRequest.TabIndex = 20;
+            this.btnRequest.Text = "Zahtjevi";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(472, 236);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(169, 41);
+            this.btnEdit.TabIndex = 21;
+            this.btnEdit.Text = "Izmijeni podatke";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteCategory
+            // 
+            this.btnDeleteCategory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeleteCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteCategory.Location = new System.Drawing.Point(472, 283);
+            this.btnDeleteCategory.Name = "btnDeleteCategory";
+            this.btnDeleteCategory.Size = new System.Drawing.Size(169, 41);
+            this.btnDeleteCategory.TabIndex = 22;
+            this.btnDeleteCategory.Text = "Obriši kategoriju";
+            this.btnDeleteCategory.UseVisualStyleBackColor = true;
+            // 
+            // frmCategories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 450);
+            this.Controls.Add(this.btnDeleteCategory);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnRequest);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSaveAsPDF);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.dgvCategory);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmCategories";
+            this.Text = "Kategorije";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Button btnSaveAsPDF;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDeleteCategory;
+    }
+}
